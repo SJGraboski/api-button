5ac643031681fe26f237364d46f0700e34691d81
Steven Graboski <sgrabosk@gmail.com>
2016-03-08 16:06:14 -0500

    Just need to check WC3 Validity

--- assets/js/api.js
+++ assets/js/api.js
@@ -2,7 +2,8 @@
  * ============== */
 
 // Array for default buttons
-var buttons = ["Hitchock", "Trufaut"];
+var buttons = ["Tabby Cat", "Black Cat", "Persian Cat", "Maine Coon",
+							 "Siamese Cat", "Oriental Cat", "Sphinx Cat", "Bengal Cat", "Kittens!"];
 
 /* 2: Functions / Objects
  * ====================== */
@@ -35,32 +36,117 @@ var btnizer = {
 		// step four: spawn the buttons
 		for (var i = 0; i < this.buttons.length; i++){
 			var theButton = $('<button>').text(this.buttons[i])
-										.attr("data-name", this.buttons[i]);
-			$('#button-box').prepend(theButton);
+											.attr("data-name", this.buttons[i])
+											.addClass("gif-button")
+											.addClass("btn")
+											.addClass("btn-info")
+											.addClass("btn-sm");
+			$('#button-box').append(theButton);
 		}
 	},
 
 	// call api on pressing new buttons
 	powerButton : function(sel) {
 
+		// empty the gif-zone
+		$('#gif-zone').empty();
+
+		// catch search key
+		var apiSearch = sel.data("name");
+
+		// make query url with search key
 		var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + 
 									apiSearch + 
-									"&api_key=dc6zaTOxFJmzC&limit=1"
+									"&api_key=dc6zaTOxFJmzC&limit=10";
+
+		// make the api call
+		$.ajax({url: queryURL, method: "GET"})
+		.done(function(response){
+			
+			// make a shorthand for response
+			var pool = response.data;
+
+			// run through the pool array, which will have a max length of 10
+			for (var i = 0; i < pool.length; i++) {
+
+				/* 1: Catch the info
+				 * ================= */
+
+				// catch the still image url
+				var still = pool[i].images.fixed_height_still.url;
+
+				// catch the animated image url
+				var animated = pool[i].images.fixed_height.url;
+
+				// display rating
+				var rating = pool[i].rating;
+				if (!rating) {
+					rating = "none";
+				}
+
+				/* 2: Format the info
+				 * ================= */
+
+				// construct the div
+				var gifBox = $('<div>').addClass("gif-box");
+
+				var gifImg = $('<img>').addClass("giffy")
+											.addClass("img-responsive")
+											.attr('src', still)
+											.attr("data-still", still)
+											.attr("data-animated", animated)
+											.attr("data-state", "still");
+
+				var gifRating = $('<p>').addClass('gif-rating')
+												.text("Rating: " + rating);
+
+				gifBox.append(gifImg, gifRating);
+
+				/* 3: Display the info
+				 * =================== */
+				$('#gif-zone').prepend(gifBox);
+
+			}
+		})
+	},
+
+	picSwitcher : function(sel) {
+		// grab data of this
+		var state = sel.attr("data-state");
+		if (state === "still") {
+			console.log("gif animated");
+			var animateUrl = sel.data("animated");
+			sel.attr('src', animateUrl);
+			sel.attr("data-state", "animated");
+		}
+		else if (state === "animated") {
+			console.log("gif frozen")
+			var stillUrl = sel.data("still");
+			sel.attr('src', stillUrl);
+			sel.attr("data-state", "still");
+		}
 	}
-
-
 }
 
 /* 3: Calls
  * ======== */
 
-$(document).ready(function() {
+$("#side").ready(function() {
 	btnizer.addButton();
-	console.log(btnizer.buttons);
 });
 
-$(document).on("click", "#add-it", function() {
+$("form").on("click", "#add-it", function() {
 	btnizer.addButton();
 	// prevent page from refreshing
 	return false;
 });
+
+$("#gif-zone").on("click", ".giffy", function() {
+	btnizer.picSwitcher( $(this) );
+});
+
+$("#button-box").on("click", ".gif-button", function() {
+	btnizer.powerButton( $(this) );
+});
+
+
